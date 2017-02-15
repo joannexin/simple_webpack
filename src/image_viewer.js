af4983342cbdb20e2d6731f45ecc923b5082ab49
Joanne <jeffiefeifei@gmail.com>
2017-02-15 15:38:58 -0800

    [add] click button

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,13 +1,25 @@
-import big from '../assets/big.jpg';
+// import big from '../assets/big.jpg';
+// import small from '../assets/small.jpg';
+// import '../styles/image_viewer.css';
+
+// const image = document.createElement('img');
+// image.src = small;
+
+// document.body.appendChild(image);
+
+// const bigImage = document.createElement('img');
+// bigImage.src = big;
+
+// document.body.appendChild(bigImage);
+
+// ==========================================
+
 import small from '../assets/small.jpg';
 import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+	const image = document.createElement('img');
+	image.src = small;
 
-document.body.appendChild(image);
-
-const bigImage = document.createElement('img');
-bigImage.src = big;
-
-document.body.appendChild(bigImage);
+	document.body.appendChild(image);
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@
 // const sum = require('./sum');
 
 // ES6 version of import
-import sum from './sum';
-import './image_viewer';
+// import sum from './sum';
+// import './image_viewer';
 
-const total = sum(10, 5);
-console.log(total);
+// const total = sum(10, 5);
+// console.log(total);
+
+// ======================================
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
+	System.import('./image_viewer').then(module => {
+		module.default();
+	});
+};
+
+document.body.appendChild(button);
