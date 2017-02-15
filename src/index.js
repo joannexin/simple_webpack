9cc3fdb346e04eca5787d3be7e9f7a804ad5d269
Joanne <jeffiefeifei@gmail.com>
2017-02-14 17:27:00 -0800

    [add] css in bundle.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-// ES6 version of import
-import sum from './sum';
 // ES5 version of import
 // const sum = require('./sum');
+
+// ES6 version of import
+import sum from './sum';
+import './image_viewer';
 
 const total = sum(10, 5);
 console.log(total);
